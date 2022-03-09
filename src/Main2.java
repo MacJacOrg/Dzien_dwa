b33cada2e3567374c4df7b963fe0fe86b6f5faca
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-09 19:54:51 +0100

    add if&&else file

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        if (number<10 || number>15) {
+            System.out.println("to jest prawda");
+        } else {
+            System.out.println("to jest fałsz");
+        }
+    }
+}
