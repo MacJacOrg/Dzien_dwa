653ea29bb0e8a595792e520523952a5a70b9fec4
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-09 19:41:53 +0100

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner skanuje = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String name = skanuje.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String lastName = skanuje.nextLine();
+        System.out.println("Cześć "+ name + lastName);
+    }
 }
