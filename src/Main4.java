637e7581e22d09f822c60acf8f7d929a88ae73ae
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-09 20:25:12 +0100

    add tablica file

--- src/Main4.java
+++ src/Main4.java
@@ -1,2 +1,8 @@
 public class Main4 {
+    public static void main(String[] args) {
+        int tablicaCyfr[] = new int[5];
+        tablicaCyfr = new int[]{1, 2, 3, 4, 5};
+
+        System.out.println(tablicaCyfr[2]);
+    }
 }
