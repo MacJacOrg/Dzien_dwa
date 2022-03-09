d975d72cafffe4d25945d0bef1891722bc847347
maciejJachimowicz <99193047+MacJacOrg@users.noreply.github.com>
2022-03-09 20:47:29 +0100

    add for(petla) file

---
+++ src/Main5.java
@@ -0,0 +1,9 @@
+public class Main5 {
+    public static void main(String[] args) {
+        String tablicaStringow[] = new String[4];
+        tablicaStringow = new String[]{"Dolar", "Abi", "Bols", "Szarik"};
+        for (int i = 0; i < 4; i++){
+            System.out.println(tablicaStringow[i]);
+        }
+    }
+}
